372d7c6c4603963cd895f9e3f1162aeb1f31e5e7
Phil Guinchard <phil.guinchard@gmail.com>
2024-05-29 00:27:37 -0400

    General fixes and virsh integration
     - You can tell the script to automatically backup all your VMs by
       passing "*" (with quotes) to the -d flag
     - The script checks for the existence of a given VM before attempting a
       backup.  Prints a warning message if the VM is not located.
     - Various tweaks and improvements

--- vm-snapshot.sh
+++ vm-snapshot.sh
@@ -20,57 +20,68 @@
 #
 # Will perform an incremental backup (if possible) of a running or stopped VM 
 # and deposit it into a monthly backup directory.  On the 15th of the current 
-# month, the previous months' snapshots are deleted. This means there will be 
+# month, the previous month's snapshots are deleted. This means there will be 
 # about 2-6 weeks of backups at any given time.
 
 # Creates a lock so that only one instance of this script may be executed
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -xn "$0" "$0" "$@" || :
 
-print_help () {
-    printf "Performs an incremental backup (if possible) on the given VMs.\n\n"
-    printf "Usage:\n  -v [VM_NAMES]\n  -o [PATH]\n\n"
-    printf "Example: vm-snapshot -v vm1,vm2,vm3 -o /tmp/backups\n"
-}
+BACKUP_TOOL=`which virtnbdbackup`
 
-BACKUP_DIR=""
+VIRSH=`which virsh`
 
-while getopts ':v:o:h' OPTION; do
-    case "$OPTION" in    
-        v)
-            IFS=',' read -r -a VMs <<< "$OPTARG"
-        ;;
-        o) 
-            BACKUP_DIR=$OPTARG
-        ;;
-	h)
-            print_help
-
-	    exit 0
-	;;
-        \?)
-            printf "Invalid usage: \n\n-v [VM_NAMES] \n\n-o [PATH]\n\n"
-	    printf "Example: vm-snapshot -v vm1,vm2,vm3 -o /tmp/backups"
-        ;;
-    esac
-done
-
-shift "$(($OPTIND -1))"
-
-if [ ${#VMs[@]} -eq 0 ] || [ ! -d $BACKUP_DIR  ]; then
-    printf "Please supply at least one VM name and an output dir "
-    printf "(vm-snapshot -v vm1 -o /tmp/backups).\n\n"
-    printf "Multiple VMs may be passed with commas (-v vm1,vm2,etc...).\n"
+if [ -z "${VIRSH}" ]; then
+    printf "Could not find virsh.\n"
 
     exit 1
 fi
-
-BACKUP_TOOL=`which virtnbdbackup`
 
 if [ -z "${BACKUP_TOOL}" ]; then
     printf "Could not find virtnbdbackup.\n\n"
     printf "https://github.com/abbbi/virtnbdbackup\n"
 
-    exit 2 
+    exit 99
+fi
+
+print_help () {
+    printf "Performs an incremental backup (if possible) on the given VMs.\n\n"
+    printf "Usage:\n  -d [VM_NAME(S) or \"*\"]\n  -o [PATH]\n\n"
+    printf "Example: vm-snapshot -d vm1,vm2,vm3 -o /tmp/backups\n"
+}
+
+BACKUP_DIR=""
+
+while getopts ':d:o:h' OPTION; do
+    case "${OPTION}" in    
+        d)
+	    VM_LIST="${OPTARG}"
+	    
+	    if [ "${VM_LIST}" = "*" ]; then
+		VM_LIST=`${VIRSH} list --all --name | /usr/bin/paste -sd "," -`
+            fi
+
+	    IFS=',' read -r -a VMs <<< "${VM_LIST}"
+        ;;
+        o) 
+            BACKUP_DIR=${OPTARG}
+        ;;
+	h|\?)
+            print_help
+
+	    exit 0
+	;;
+    esac
+done
+
+shift "$(($OPTIND -1))"
+
+if [ ${#VMs[@]} -eq 0 ] || [ ! -d ${BACKUP_DIR}  ]; then
+    printf "Please supply at least one VM name and an output dir "
+    printf "(vm-snapshot -d vm1 -o /tmp/backups).\n\n"
+    printf "Multiple VMs may be passed with commas (-d vm1,vm2,etc...).  "
+    printf "Use \"*\" (with quotes) to select all VMs.\n"
+
+    exit 100
 fi
 
 DAY_OF_MONTH=`date +'%d'`
@@ -79,27 +90,29 @@ SNAPSHOT_NAME=`date +'%Y-%m'`
 
 # Backup all the VMs, running or stopped, incrementally if possible
 for name in ${VMs[@]}; do
+    ${VIRSH} domstate ${name} > /dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+	printf "WARNING: Could not find a VM named \"${name}\"\n"
+    
+        continue
+    fi
+
     SNAPSHOT_PATH=${BACKUP_DIR}/${name}/${SNAPSHOT_NAME}
 
     printf "Backing up ${name} to ${SNAPSHOT_PATH}\n"
    
-    START_VM=""
-
-    if [ ! -d ${SNAPSHOT_PATH}  ]; then
-        START_VM=" -S"
-    fi
-
-    ${BACKUP_TOOL}${START_VM} --noprogress -d ${name} -l auto -o ${SNAPSHOT_PATH}
+    ${BACKUP_TOOL} -S --noprogress -d ${name} -l auto -o ${SNAPSHOT_PATH}
 
     # Delete last month's backups if our latest backup succeeded and it is the
     # middle of the current month.
-    if [ $? -eq 0 ] && [ $DAY_OF_MONTH -eq 15 ]; then
+    if [ $? -eq 0 ] && [ ${DAY_OF_MONTH} -eq 15 ]; then
         LAST_MONTH=`date -d "$(date +%Y-%m-1) -1 month" +%Y-%m`
     
     	LAST_MONTHS_BACKUPS_DIR=${BACKUP_DIR}/${name}/${LAST_MONTH}
 
         if [ -d ${LAST_MONTHS_BACKUPS_DIR} ]; then
-            printf "Removing backups for ${name} for ${LAST_MONTH}"
+            printf "Removing backups for ${name} for ${LAST_MONTH}\n"
 	
             rm -rf ${LAST_MONTHS_BACKUPS_DIR}
         fi
